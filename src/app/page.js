459da9f6a8cce9b60f73b5f2cba25b1e873d0a01
yyh0215 <yangyinghui0215@outlook.com>
2025-05-11 12:30:52 +0800

    zzz

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
 export default function Hello() {
-  return
-  <div>
-    <h1>Hello</h1>
-  </div>
+  return (
+    <div>
+      <h1 style={{
+        color: 'red',
+        textAlign: 'center',
+        width: '100%',
+        margin: '20px 0', 
+        textShadow: '3px 3px 6px rgba(128, 128, 128, 0.8)'
+      }}>Hello
+      </h1>
+    </div>
+  );
 }
